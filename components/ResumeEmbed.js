f3736931f06f3661a30264e7341b190b0abcc329
WPoulos24 <xeverest3dX@gmail.com>
2023-02-22 07:41:18 -0500

    Update Resume 2
    - Another update to the resume

--- components/ResumeEmbed.js
+++ components/ResumeEmbed.js
@@ -5,7 +5,7 @@ function ResumeEmbed() {
     <div className="w-full -mt-8">
       <iframe
         className="w-full"
-        src="https://docs.google.com/document/d/153p-wpQEeNCAr35Q4tHrvD5ti-wbwZrrqWveuwVlejw/edit?usp=sharing"
+        src="https://docs.google.com/document/d/153p-wpQEeNCAr35Q4tHrvD5ti-wbwZrrqWveuwVlejw/preview"
         height={580}
       ></iframe>
     </div>
