38cfeb1785c8447c7eb67d1711aabd145b64678b
WPoulos24 <xeverest3dX@gmail.com>
2023-02-28 14:49:29 -0500

    Vercel Update

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import "tailwindcss/tailwind.css";
 import Layout from "../components/Layout";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps, router }) {
   return (
     <Layout className="bg-bg">
       <Component {...pageProps} key={router.route} />
+      <Analytics />
     </Layout>
   );
 }
